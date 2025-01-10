db1c46a63049813831aaef41ed0ca8a85c9b62d2
lemongooo <71778468+lemongooo@users.noreply.github.com>
2025-01-10 19:25:35 +0200

    Update fetch_spotify_data.py

--- src/fetch_spotify_data.py
+++ src/fetch_spotify_data.py
@@ -70,27 +70,6 @@ if __name__ == "__main__":
         "artist_avg_energy": 0,
         "duration_minutes": 0
     }
-
-    # # 遍歷 Top Tracks 並打印完整欄位
-    # for i, track in enumerate(top_tracks, 1):
-    #     track_id = track["id"]  
-    #     track_name = track["name"]  
-    #     artists = ", ".join(artist["name"] for artist in track["artists"]) 
-    #     popularity = track["popularity"]
-
-    #     # 輸出主要欄位
-    #     print(f"{i}. track_id: {track_id}")
-    #     print(f"   track_name: {track_name}")
-    #     print(f"   artist_name: {artists}")
-    #     print(f"   popularity: {popularity}")
-
-    #     # 輸出額外欄位
-    #     for field, default_value in extra_fields.items():
-    #         print(f"   {field}: {default_value}")
-    #     print("-" * 50)
-    #------------------------
-
-    # 整理數據存入 DataFrame
     data = []
     for track in top_tracks:
         normalized_popularity = track["popularity"] / 100.0  
@@ -100,7 +79,7 @@ if __name__ == "__main__":
             "artist_name": ", ".join(artist["name"] for artist in track["artists"]),
             "popularity": normalized_popularity
         }
-        row.update(extra_fields)  # 添加額外欄位
+        row.update(extra_fields)
         data.append(row)
 
     df = pd.DataFrame(data)
@@ -108,7 +87,6 @@ if __name__ == "__main__":
     df.to_csv("output.csv", index=True)
     print("Data saved to output.csv")
 
-    # 登錄到 Hopsworks
     os.environ["HOPSWORKS_API_KEY"] = "K86PR55oWoqOXhUX.VUJFWq1zEqfhZntoebWp1RLsH90VVbAYuqukvh5AcM8xHOYk7c4vg4VvD6iZEG37"
     project = hopsworks.login()
     fs = project.get_feature_store()
@@ -116,24 +94,11 @@ if __name__ == "__main__":
     print(project.name)
 
 
-
-
-    # # 获取所有 Feature Groups 的名称（替代 get_feature_groups）
-    # feature_group_names = [fg.name for fg in fs.get_feature_groups()]
-    # print("Available Feature Groups:")
-    # print(feature_group_names)
-
     
     feature_group = fs.get_feature_group(name="spotify_features", version=2)
     print("Selected Feature Group:")
     print(feature_group)
 
-    # schema = feature_group.schema
-    # print("Feature Group Schema (Raw):")
-    # print(schema)
-    
-    #----------
-    # 将需要转换的数据列统一转换为 float
     columns_to_convert = [
         "acousticness", "danceability", "energy", "loudness", "speechiness", 
         "instrumentalness", "liveness", "tempo", "time_signature", 
@@ -142,16 +107,14 @@ if __name__ == "__main__":
         "duration_minutes"
     ]
 
-    # 转换列为 float 类型
     df[columns_to_convert] = df[columns_to_convert].astype(float)
 
-    # 打印检查转换后的数据类型
     print("Data types after conversion:")
     # print(df.dtypes)
 
 
 
-    # 上傳數據到 Feature Group
+
     feature_group.insert(df, write_options={"wait_for_job": True})
     print("Data uploaded to Hopsworks successfully!")
 
